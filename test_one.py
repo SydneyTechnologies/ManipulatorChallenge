140eff9442638eb4558626d3f764bfb772df5487
sydneyIdundun <sydney.idundun@gmail.com>
2023-01-05 10:56:31 +0400

    chore: created a working script for OBD
    OBD stands for object detection, the code takes the 20th frame and then performs object detection against a list of 80 elements within the coco names list

--- test_one.py
+++
@@ -1,5 +0,0 @@
-# the main aim of this test_one script will be to make use 
-# of opencv to first identify an object
-# Draw the bounding box around the image 
-# then finally get the dimensions of the image
-# If possible translate that information into real world coordinates
